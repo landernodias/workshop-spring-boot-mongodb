4a636b16d0f150b3fa6e971fb03efee56e79eed6
Nelcione Dias <nelcionedias@gmail.com>
2022-07-05 12:36:31 -0300

    Using DTO pattern to return users

---
+++ src/main/java/com/nelcionedias/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,47 @@
+package com.nelcionedias.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.nelcionedias.workshopmongo.domain.User;
+
+public class UserDTO  implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String email;
+	
+	public UserDTO() {	
+	}
+	
+	//copy user from DTO
+	public UserDTO(User obj) {
+		this.id = obj.getId();
+		this.name = obj.getName();
+		this.email = obj.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
