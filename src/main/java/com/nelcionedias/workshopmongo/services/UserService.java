c742a8a4b9c1748feec1f72ec22ae478341d11c4
Nelcione Dias <nelcionedias@gmail.com>
2022-07-05 14:57:04 -0300

    User insertion with POST

--- src/main/java/com/nelcionedias/workshopmongo/services/UserService.java
+++ src/main/java/com/nelcionedias/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelcionedias.workshopmongo.domain.User;
+import com.nelcionedias.workshopmongo.dto.UserDTO;
 import com.nelcionedias.workshopmongo.repository.UserRepository;
 import com.nelcionedias.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
